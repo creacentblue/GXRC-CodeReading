d9c629291aeb251cb45ebb312bc13c75c4a67118
TseyuW <981891344@qq.com>
2021-10-14 17:23:16 +0800

    Create libdbBinPart.h

---
+++ v650/db/libdbBinPart.h
@@ -0,0 +1,10 @@
+0000-0015
+  header
+0016-0031
+  dbBinId2D
+    m_row;m_col
+0032-0097
+  dbBinPart
+    s_dbBinPart;m_topBBox;m_dbu;m_tileW;m_tileH;m_numRow;m_numCol;m_isInit;m_binBBoxVV
+
+#finished
