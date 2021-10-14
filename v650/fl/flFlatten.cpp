29445e9abfc715e28a9cd019c33566e63613b510
TseyuW <981891344@qq.com>
2021-10-14 20:05:04 +0800

    Create flFlatten.cpp

---
+++ v650/fl/flFlatten.cpp
@@ -0,0 +1,13 @@
+0000-0015
+	header
+0016-0035
+	flFlatten(topDesign,halo,binId,OA_FLOW)
+	CCI flow constructor
+0036-0055
+	flFlatten(cimain,halo,binId,CCI_FLOW)
+		CCI flow constructor
+0056-0116
+	run()
+		create flFlattenOAjob or flFlattenCCIjob and run
+
+#finished
