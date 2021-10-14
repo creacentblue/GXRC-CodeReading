3fc59c5e03b59b4f4f5ecdf4f226ece90e2bd2c6
TseyuW <981891344@qq.com>
2021-10-14 19:34:32 +0800

    Update dbBinPart.cpp

--- v650/db/dbBinPart.cpp
+++ v650/db/dbBinPart.cpp
@@ -3,10 +3,19 @@
 0018-0032
 	dbBinPart()
 		init dbBinPart
+
+0070-0079
+	getBinId2D(binId,row//out,col//out)
+
 0148-0239
 	load(string filename)
 		open file(.gex/partitioninfo);
 		load data(dbu,topBBox,numRow,numCol,binBBoxVV) from file
 		set tileW,tileH,isInit
+
+
+0331-0343
+	getBinBBox(binId)
+		
 
     
