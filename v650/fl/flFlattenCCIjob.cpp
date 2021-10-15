ed0f63945f2fe631e6047b9d1ae6f5fd2f36fd00
TseyuW <981891344@qq.com>
2021-10-15 09:03:50 +0800

    Update flFlattenCCIjob.cpp

--- v650/fl/flFlattenCCIjob.cpp
+++ v650/fl/flFlattenCCIjob.cpp
@@ -1,6 +1,50 @@
 0001-0036
-  Header&namespace
+	header
 0037-0093
-  flFlattenCCIjob : ciMain,halo,binId
-    
-  
+	flFlattenCCIjob(ciMain,halo,binId)
+		constructor
+0094-0101
+	~flFlattenCCIjob()
+  		deconstructor
+0102-0146
+	run()
+		flNetPinCount.computeCellzFlattenNetTermPin()
+		flCellShapesCCI.mergeCellShapes()
+		loadValidNetIds()
+		buildSkipInstHash()
+		startFlatten()
+		m_skipInstHash=NULL
+0147-0191
+	
+0192-0231
+	startFlatten
+0232-0386
+	flattenRcv
+0387-0494
+	flattenShapes
+0495-0508
+	isOverlappedWithHaloBBox(geTrapz)
+0509-0643
+	
+0644-0753
+	
+0754-0763
+	
+0764-0798
+	
+0799-0820
+	
+0821-0832
+
+0833-0862
+	
+0863-0875
+	
+0876-0898
+	
+0899-0931
+	
+0932-0954
+	
+0955-0992
+	debug mode
